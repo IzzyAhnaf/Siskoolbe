d306221752a07f9bbb2485065d3bdf8e5ccc2d11
unknown <markoro1234@gmail.com>
2024-04-23 23:14:47 +0700

    c

--- Front/src/Pages/AboutUs.jsx
+++ Front/src/Pages/AboutUs.jsx
@@ -1,35 +1,46 @@
-const AboutUS = ({WMobile}) => {
-    return(
+import CardZev from "../components/universal/febryan";
+import CardGilang from "../components/universal/gilang";
+import CardIzzy from "../components/universal/izzy";
+import CardIpul from "../components/universal/saipul";
+
+import React from "react";
+
+const AboutUS = ({ WMobile }) => {
+    const [isFlipped, setIsFlipped] = React.useState(false);
+
+    const handleMouseEnter = () => {
+        setIsFlipped(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsFlipped(false);
+    };
+    return (
         <>
-        {!WMobile ? (
-            <div className="flex flex-col w-full rounded-xl bg-[#D9D9D9] mx-4 p-8 font-inter overflow-y-auto slim-scroll">
-                <div className="flex flex-col items-center w-full space-y-4 mb-32">            
-                    <h1 className="text-4xl font-bold">How it Started</h1>
-                    <p className="lg:text-2xl md:text-xl lg:w-[40%] md:w-1/3 text-center">Selamat datang di website absensi resmi SMKN 1 Depok! 
-                    Kami hadir untuk mempermudah dan mempercepat proses 
-                    pencatatan kehadiran siswa dan tenaga pendidik. Dibangun 
-                    dengan teknologi terkini dan fitur-fitur canggih, website 
-                    kami menjaga keamanan data sebagai prioritas utama. 
-                    Dukungan dan kerjasama dari komunitas SMKN 1 Depok sangat berarti bagi kami. Terima kasih atas kunjungan Anda!
+            {!WMobile ? (
+                <div className="flex flex-col w-full rounded-xl bg-[#D9D9D9] mx-4 p-8 font-inter overflow-y-auto slim-scroll">
+                    <div className="flex flex-col items-center w-full space-y-4 mb-32">
+                        <h1 className="text-4xl font-bold">How it Started</h1>
+                        <p className="lg:text-2xl md:text-xl lg:w-[40%] md:w-1/3 text-center">Selamat datang di website absensi resmi SMKN 1 Depok!
+                            Kami hadir untuk mempermudah dan mempercepat proses
+                            pencatatan kehadiran siswa dan tenaga pendidik. Dibangun
+                            dengan teknologi terkini dan fitur-fitur canggih, website
+                            kami menjaga keamanan data sebagai prioritas utama.
+                            Dukungan dan kerjasama dari komunitas SMKN 1 Depok sangat berarti bagi kami. Terima kasih atas kunjungan Anda!
 
-                    Salam, Tim Pengembang Website Absensi SMKN 1 Depok</p>
-                </div>
-                <div className="flex space-x-48 justify-center">
-                    <div className="rounded-md bg-white w-1/4">
-                        
+                            Salam, Tim Pengembang Website Absensi SMKN 1 Depok</p>
                     </div>
-                    <div className="rounded-md bg-white w-1/4">
-
-                    </div>
-                    <div className="rounded-md bg-white w-1/4">
-
+                    <div className="flex flex-row space-x-12 items-center justify-center ">
+                        <CardGilang />
+                        <CardZev />
+                        <CardIzzy />
+                        <CardIpul />
                     </div>
                 </div>
-            </div>
-        ):(
-            <>
-            </>
-        )}
+            ) : (
+                <>
+                </>
+            )}
         </>
     )
 }
---
+++ Front/src/components/universal/saipul.jsx
@@ -0,0 +1,50 @@
+import '../Styling.css';
+import React from 'react';
+import berdua from '../../assets/berdua.png';
+
+function CardIpul() {
+    const [isFlipped, setIsFlipped] = React.useState(false);
+
+    const handleMouseEnter = () => {
+        setIsFlipped(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsFlipped(false);
+    };
+
+    return (
+        <div className='flex-row'>
+            <div
+                className={`flip-card ${isFlipped ? 'is-flipped' : ''}`}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+            >
+                <div className="flip-card-inner border-1 rounded-[20px] ">
+                    <div className="flip-card-front-gilang flex justify-center items-center border-1 rounded-lg  ">
+                        <div className=" flex-col text-center text-white bg-blue-500 mt-[300px] py-2 items-center  justify-center px-20 rounded-md ">
+                            <h1>Saipul</h1>
+                            <h1>
+                                Front-End Dev
+                            </h1>
+                        </div>
+                    </div>
+
+
+                    <div className="flip-card-back border-1 bg-white rounded-[20px] mt-[-364px]">
+                        <div className="bg-berdua flex justify-center items-center">
+                            <img src={berdua} alt="Berdua" className="rounded-full w-40 h-40 mt-[50px] object-cover bg-white" />
+                        </div>
+                        <div className="text-center  text-black mt-[40px] ">
+                            <p className="text-lg font-semibold">Nama: Syaiful Ilham</p>
+                            <p className="text-md">Quotes: "Quote goes here"</p>
+                            <p className="text-md">Email: @email.com</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div >
+    );
+}
+
+export default CardIpul;
