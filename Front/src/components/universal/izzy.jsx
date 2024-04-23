07420b24f569d0afda8b7efecdb670b5077fecd0
unknown <markoro1234@gmail.com>
2024-04-24 00:08:00 +0700

    c

--- Front/src/components/universal/izzy.jsx
+++ Front/src/components/universal/izzy.jsx
@@ -1,6 +1,7 @@
 import '../Styling.css';
 import React from 'react';
-import berdua from '../../assets/berdua.png';
+import izzy from "../../assets/izzy.png"
+
 
 function CardIzzy() {
     const [isFlipped, setIsFlipped] = React.useState(false);
@@ -20,8 +21,8 @@ function CardIzzy() {
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
             >
-                <div className="flip-card-inner border-1 rounded-[20px] ">
-                    <div className="flip-card-front-gilang flex justify-center items-center border-1 rounded-lg  ">
+                <div className="flip-card-inner-izzy border-1 rounded-[20px] ">
+                    <div className="flip-card-front flex justify-center items-center border-1 rounded-lg  ">
                         <div className=" flex-col text-center text-white bg-blue-500 mt-[300px] py-2 items-center  justify-center px-20 rounded-md ">
                             <h1>Izzy</h1>
                             <h1>
@@ -31,9 +32,9 @@ function CardIzzy() {
                     </div>
 
 
-                    <div className="flip-card-back border-1 bg-white rounded-[20px] mt-[-364px]">
-                        <div className="bg-berdua flex justify-center items-center">
-                            <img src={berdua} alt="Berdua" className="rounded-full w-40 h-40 mt-[50px] object-cover bg-white" />
+                    <div className="flip-card-back border-1 bg-white rounded-[20px] ">
+                        <div className=" flex justify-center items-center">
+                            <img src={izzy} alt="" className="rounded-full w-40 h-40 mt-[50px] object-cover bg-white" />
                         </div>
                         <div className="text-center  text-black mt-[40px] ">
                             <p className="text-lg font-semibold">Nama: Izzy Ahnaf</p>
